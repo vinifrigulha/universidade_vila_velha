d1b9e0c2cac1dcbeeb9691c2e3a9d29870976fc5
Isaque Ferreira <95030412+Fisaq@users.noreply.github.com>
2023-11-13 17:25:24 -0300

    Create verifica_ordem_vetor.c

---
+++ Estrutura de Dados/Resoluções /verifica_ordem_vetor.c
@@ -0,0 +1,41 @@
+//Crie um programa que leia um vetor de 10 elementos inteiros e verifique se ele está em ordem crescente
+
+int recebeVetor(int vetor[10]){
+
+  printf("Informe os valores do vetor...\n");
+  
+  for(int c = 0; c < 10; c++){
+ 
+    printf("\nElemento %d: ", c+1);
+    scanf("%d", &vetor[c]);
+    
+    }
+  
+  int ordemCrescente = 0;
+
+  // Verifica se o vetor está em ordem crescente
+  for(int i = 0; i < 9; i++){
+
+    if (vetor[i] > vetor[i +1]){ 
+      ordemCrescente = 0;
+      break;
+    }   
+  }
+  
+  if (ordemCrescente){
+    printf("O vetor não está em ordem crescente.");
+  }
+  else{
+    printf("O vetor está em ordem crescente.");
+  }
+
+  return 0;
+}
+
+int main(){
+
+  int vetor[10];
+  recebeVetor(vetor);
+
+  return 0;
+}
