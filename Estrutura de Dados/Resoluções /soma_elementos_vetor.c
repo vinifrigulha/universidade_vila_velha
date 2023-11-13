1d777312df2d2cae138fb986636dc178ede223f7
Isaque Ferreira <95030412+Fisaq@users.noreply.github.com>
2023-11-13 17:24:20 -0300

    Create soma_elementos_vetor.c

---
+++ Estrutura de Dados/Resoluções /soma_elementos_vetor.c
@@ -0,0 +1,33 @@
+//Crie um programa que leia 10 números inteiros do usuário e armazene-os em um vetor. Em seguida, exiba a soma de todos os elementos do vetor.
+
+int somaVetor(int num){
+  
+  int vetor[10];
+  int soma = 0;
+  
+  //intera os valores recebidos no vetor e soma
+  for(int c = 0; c <10;c++){
+    vetor[c] = num;  
+    soma += vetor[c]; 
+  }
+
+  return soma;
+}
+
+int main(){
+
+  int num = 0;
+  int cont = 0;
+  
+  while (cont <= 9){
+    
+    printf("\nFavor, informe um número inteiro: ");
+    scanf("%d", &num);
+    printf("\nValor informado: %d\n", num);
+    cont++;
+  }
+  int soma = somaVetor(num);
+  printf("\nSoma dos valores = %d ", soma);
+
+  return 0;
+}
