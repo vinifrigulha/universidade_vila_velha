bc6c05a1f4ea1df89ae9d678722ded580e52a41c
Isaque Ferreira <95030412+Fisaq@users.noreply.github.com>
2024-05-27 22:17:41 -0300

    Update mpi_pi.c

--- Arquitetura de Computadores 2/Trabalho B2 - Cluster/mpi_pi.c
+++ Arquitetura de Computadores 2/Trabalho B2 - Cluster/mpi_pi.c
@@ -1,13 +1,36 @@
 #include <mpi.h>
 #include <stdio.h>
+#include <math.h>
 
 int main(int argc, char** argv) {
+    int rank, size, n = 1000000, i;
+    double h, sum = 0.0, pi, x;
 
-    // Inicializa o ambiente MPI (não precisamos dos argumentos de linha de comando
-    // nesse exemplo simples:
-    MPI_Init(NULL, NULL);
+    // Initialize the MPI environment
+    MPI_Init(&argc, &argv);
 
-    // Obtém o número de processos:
+    // Get the rank of the process
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    // Get the number of processes
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    // Calculate the width of each rectangle
+    h = 1.0 / (double) n;
+    
+    // Each process calculates a part of the sum
+    for (i = rank; i < n; i += size) {
+        x = h * ((double)i + 0.5);
+        sum += 4.0 / (1.0 + x * x);
+    }
+
+    // Multiply by the width of the rectangles
+    sum = h * sum;
+
+    // Reduce all of the local sums into the global sum
+    MPI_Reduce(&sum, &pi, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+
+ // Obtém o número de processos:
     int nprocs = 0;
     MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
 
@@ -23,10 +46,10 @@ int main(int argc, char** argv) {
     // Imprime um "Olá, mundo!":
     printf("Olá, mundo! Eu sou o processador %2d em %s, de um total de %2d processadores.\n",
            procid, nome_do_processo, nprocs);
+    printf("Valor aproximado de Pi: %.16f\n", pi);
 
-    // Finaliza o ambiente MPI:
+    // Finalize the MPI environment
     MPI_Finalize();
 
-    // Retorna
     return 0;
 }
